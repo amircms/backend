ca79bf3e80feddb4e9f812599846ed81289a089c
Amir Maghami <Maghami.A1987@gmail.com>
2025-04-16 14:53:56 +0330

    remove module

--- src/nested-menu-items/dto/update-nested-menu-item.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateNestedMenuItemDto } from './create-nested-menu-item.dto';
-
-export class UpdateNestedMenuItemDto extends PartialType(
-  CreateNestedMenuItemDto,
-) {}
